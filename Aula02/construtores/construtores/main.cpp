eb9c308534d82395525532259c92ff46be8e2e3c
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-05-15 15:34:48 -0300

    Construtores

---
+++ Aula02/construtores/construtores/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+struct A `{
+    int X;
+    A (){
+        //Construtor,,,
+        x = 0;
+        //Todo X com o OBJETO A vai inicializar com 0.
+    }
+};
+int main(){
+    A objeto;
+    printf("%d\n", objeto.x);
+    //Vai ter um valor pré definido...
+
+
+}
